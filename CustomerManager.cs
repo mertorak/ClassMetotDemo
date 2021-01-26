be611bdc750a94a523fe0c69bf3db3b438bd62c1
Mert Orak <mertorak95@outlook.com>
2021-01-26 23:13:55 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Ekleme(Customer customer)
+        {
+            Console.WriteLine(customer.Ad + " adlı müşteri eklendi. Yeni müşterimizin mesleği: " + customer.Meslek );
+        }
+        public void Silme(Customer customer)
+        {
+            Console.WriteLine(customer.Ad + " adlı müşteri silindi");
+        }
+        public void Listeleme(Customer customer)
+        {
+            Console.WriteLine("Müşterinin adı:"+ customer.Ad);
+            Console.WriteLine("Müşterinin soyadı:"+ customer.Soyad);
+            Console.WriteLine("Müşterinin ID'si:"+ customer.Id);
+            Console.WriteLine("Müşteri mesleği:"+ customer.Meslek);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Ad = "Mert";
+            customer1.Soyad = "Orak";
+            customer1.Meslek = "Software Developer";
+            customer1.Id = 1; 
+
+
+            Customer customer2 = new Customer();
+            customer2.Ad = "Berk";
+            customer2.Soyad = "Orak";
+            customer2.Meslek = "Ogrenci";
+            customer2.Id = 2;
+
+            Customer customer3 = new Customer();
+            customer3.Ad = "Maral";
+            customer3.Soyad = "Orak";
+            customer3.Meslek = "Kimyager";
+            customer3.Id = 3;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Ekleme(customer1);
+            Console.WriteLine("-----");
+            customerManager.Listeleme(customer2);
+            Console.WriteLine("-----");
+            customerManager.Silme(customer3);
+
+        }
+    }
+}
